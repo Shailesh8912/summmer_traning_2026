02b28bdcfa0e8e3c5a19a42820d97948600f4d51
shailesh8912 <saileshkumar1210@gmail.com>
2026-07-17 15:41:21 +0530

    program1

---
+++ program1.py
@@ -0,0 +1,16 @@
+
+
+name=input("enter the  name:")
+empid=int(input("enter the  Id:"))
+empdesig=input("enter Degignation:")
+empdoj=input("enter the date DD/MM/YY")
+empsal=float(input("enter the salary"))
+
+print("#"*30)
+print("employee profile ")
+print("#"*30)
+print("Employee  name:" ,name)
+print("Employee ID:",empid)
+print("Employee  degignation",empdesig)
+print("Employee doj",empdoj)
+print("Employee salary:",empsal)
