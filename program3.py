b369a53445ca0ea0ee2ebc420aa25c1d41ad66c6
shilesh <shaileshkumar1212@gmail.com>
2026-07-18 19:20:02 +0530

    program

---
+++ program3.py
@@ -0,0 +1,20 @@
+
+s1 ='welcom'
+s2 ="vikas"
+s3 ='''shiv
+ankit  
+sandeep
+deepak
+'''
+print(s1)
+print(s2)
+print(s3)
+# name ="shailesh kushwaha"
+student =[ 89,"shailesh", "bca", "subject-python","rollnumber",89 ,"collage", "goel"]
+print(student)
+
+
+ #list
+lst5 =[2,4,9,[8,9],5]
+for i in lst5:
+    print(i)
