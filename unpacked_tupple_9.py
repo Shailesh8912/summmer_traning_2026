6642429809edaf10eff6e193585f9096a5c2b242
shilesh <shaileshkumar1212@gmail.com>
2026-07-23 01:12:06 +0530

    unpackedtupple

---
+++ unpacked_tupple_9.py
@@ -0,0 +1,10 @@
+# unpackedtuples
+
+a =[(1,"siya"),(2,"pratigya maurya"), (3, "miss"),]
+
+# prints all tuples
+for i in a:
+    print(i)
+
+for roll_o,n in a:
+  print(f"roll number -{roll_o} is {n}!")
