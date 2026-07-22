ab4d2e4cd4e39965e2a0c72b38d432e6beb06e5b
shilesh <shaileshkumar1212@gmail.com>
2026-07-23 00:10:40 +0530

    program

---
+++ list6.py
@@ -0,0 +1,13 @@
+number= [x for x in range (6)]
+print(number)
+
+even=[x for x in range (12)if x%2==0]
+print(even)
+odd=[x for x in range (12)if x%2!=0]
+print(odd)
+ 
+words=[ "fatepur","kanpur","bahraich"]
+print(words)
+ 
+# upper_word=[word.upper ()for word in word:]
+# print(upper_word)
