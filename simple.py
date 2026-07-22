8d5b983afe6e40d54edcf21b7e2df695d548df0d
shilesh <shaileshkumar1212@gmail.com>
2026-07-23 00:14:46 +0530

    program

---
+++ simple.py
@@ -0,0 +1,10 @@
+# my_list =[1,2,3,4,5]
+# i = -1
+# while i>=-5:
+#      print(my_list[i],end =" ")
+#      i -= 1
+
+     # question
+my_list =["apple", "banana", "grapes","pine apple"]
+for i in range(len(my_list)):
+    print(my_list[i]+"is a frutes." ,end =" ")
